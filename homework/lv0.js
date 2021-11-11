64af7643f6dd3f3f51840d445e0e8a7cffdc6f5a
comma <2379010429@qq.com>
2021-11-11 21:32:48 +0800

    homework2

---
+++ homework/lv0.js
@@ -0,0 +1,25 @@
+// 创建对象的第一种方法
+let obj = new Object;
+    obj.name = Bigrice;
+    obj.height = 167;
+    obj.colorofskill = white;
+console.log(obj);
+
+// 创建对象的第二种方法
+let obj = {
+    name = Bigrice,
+    height = 167,
+    colorofskill = white
+}
+console.log(obj);
+
+// 创建对象的第三种方法
+function Beauty() {
+  name = Bigrice;
+   height = 167;
+    colorofskill = white
+  
+}
+
+
+
