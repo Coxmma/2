b02e25dc7b09a74c5d2c4a704f21cb73c3990c5b
comma <2379010429@qq.com>
2021-11-20 12:26:21 +0800

    homework4

---
+++ homework.4/lv0.js
@@ -0,0 +1,24 @@
+const person = {
+    name: '露露姐姐',
+    age: 1000000,
+    address: {
+      city: 'ChongQing',
+      area: 'NanShan'
+    },
+    title: ['student',{year:2021, title:'GoodStudent'}]
+  }
+  
+  const{name:userName} = person;
+  const{age:year} = person;
+  const{address: {city}} = person;
+  const{area:mountain} = person;
+  const{title:[title1]} = person;
+  const{title:[title2]} = person;
+
+  
+  console.log(userName) 
+  console.log(year) 
+  console.log(city) 
+  console.log(mountain) 
+  console.log(title1) 
+  console.log(title2) 
